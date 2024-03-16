8b4cfb84146fdc68cb8f491b92d5230c5ab078fb
eomseona <eomsa98@tukorea.ac.kr>
2024-03-16 22:05:05 +0900

    단일연결리스트 팝 메서드 실습

--- linkedList/SinglyLinkedList.js
+++ linkedList/SinglyLinkedList.js
@@ -23,6 +23,23 @@ class singlyLinkedList {
     this.length++;
     return this;
   }
+  pop() {
+    if (!this.head) return undefined;
+    var current = this.head;
+    var newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
 }
 
 // var list = new singlyLinkedList();
