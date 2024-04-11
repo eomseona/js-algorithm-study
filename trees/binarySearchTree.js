597cf033d7ec7f4865f556b982255053c9730501
eomseona <eomsa98@tukorea.ac.kr>
2024-04-11 17:33:07 +0900

    BST insert, find 메서드 구현

---
+++ trees/binarySearchTree.js
@@ -0,0 +1,48 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(val) {
+    let newNode = new Node(val);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (val === current.val) return undefined;
+      if (val < current.val) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else if (val > current.val) {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+  find(val) {
+    let current = this.root;
+    while (true) {
+      if (current === null) return false;
+      if (val === current.val) return true;
+      else if (val < current.val) current = current.left;
+      else {
+        current = current.right;
+      }
+    }
+  }
+}
