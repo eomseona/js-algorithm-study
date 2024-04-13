2f2f4a9ca8cc1704e8024499a6f9d0b0a8b1ea8c
eomseona <eomsa98@tukorea.ac.kr>
2024-04-13 22:44:30 +0900

    bfs 메서드 구현

--- trees/binarySearchTree.js
+++ trees/binarySearchTree.js
@@ -45,4 +45,18 @@ class BinarySearchTree {
       }
     }
   }
+  BFS() {
+    var node = this.root,
+      data = [],
+      queue = [];
+
+    queue.push(node);
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
 }
